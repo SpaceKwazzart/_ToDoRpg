770b48feb1488526376766aab310deb7a9ce1862
kwazzart <vladimirkwaz@gmail.com>
2023-06-05 14:54:43 +0300

    dependencies updated

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ToDoPage from './pages/ToDoPage';
 import NotFound404 from './pages/NotFound404';
 import ProfilePage from './pages/ProfilePage';
 import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
+import AboutPage from './pages/AboutPage';
+import RatingPage from './pages/RatingPage';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<ToDoPage/>}></Route>
+        <Route path='about/' element={<AboutPage/>}></Route>
+        <Route path='rating/' element={<RatingPage/>}></Route>
         <Route path='signup/' element={<SignUpPage/>}></Route>
         <Route path='signin/' element={<SignInPage/>}></Route>
         <Route path='profile/' element={<ProfilePage/>}></Route>
