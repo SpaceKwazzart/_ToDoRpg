9959682bd0f1929797662542cbc7d15f2d454989
kwazzart <vladimirbeliaev3@gmail.com>
2023-06-13 23:29:58 +0300

    Update UI components @mui *stable*

--- frontend/app/src/components/shared/MenuElement/MenuElement.jsx
+++
@@ -1,9 +0,0 @@
-import style from './MenuElement.module.css';
-
-function MenuElement({ onClick, children }) {
-    return ( 
-        <div onClick={onClick} className={style.element}>{children}</div>
-    );
-}
-
-export default MenuElement;
